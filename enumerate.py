b0a43c490971640b025c2655e7ee6462bb615901
microhuang <microhuang@live.com>
2013-09-06 22:24:37 +0800

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,29 @@
+def enumerate(collection): 
+     'Generates an indexed series:  (0,coll[0]), (1,coll[1]) ...'     
+     i = 0 
+     it = iter(collection) 
+     while 1: 
+         yield (i, it.next()) 
+         i += 1
+         
+
+count = -1 
+for count,line in enumerate(open('tags.xml','rU')): 
+    pass
+
+count += 1
+
+
+     
+count = len(open(thefilepath,'rU').readlines())
+
+
+     
+for index，text in enumerate(list)): 
+   print index ,text
+   
+   
+   
+   
+for i in range (0,len(list)): 
+    print i ,list[i]
