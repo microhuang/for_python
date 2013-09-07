762fd541f5944d160e7d314d33cd02723d3004f7
microhuang <microhuang@live.com>
2013-09-07 23:30:06 +0800

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -20,6 +20,7 @@ class Singleton(object):
   def __new__(cls, *args, **kwargs):
     if '_inst' not in vars(cls):
       cls._inst=super(Singleton, cls).__new__(cls, *args, **kwargs)
+      #cls._inst=object.__new__(cls, *args, **kwargs)
     return cls._inst
 
 
@@ -28,3 +29,35 @@ class SingleSpam(Singleton):
 
 c1=SingleSpam()
 c2=SingleSpam()
+
+
+
+
+
+class Borg(object):
+    _shared_state={}
+    def __new__(cls, *a, **k):
+        obj=object.__new__(cls, *a, **k)
+        #obj=super(Borg, cls).__new__(cls, *a, **k)
+        obj.__dict__=cls._shared_state
+        return obj
+
+class Example(Borg):
+    name=None
+    def __init__(self, name=None):
+            if name is not None: self.name=name
+    def __str__(self): return 'name->%s' % self.name
+
+a=Example('Lara')
+b=Example()
+print a,b
+        
+
+
+
+class froober(object):
+    def __init__(self):
+        pass
+    
+    
+froober=froober()
