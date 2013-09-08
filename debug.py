63bf09d6d06602228623923b4b84ff3bc53912f6
microhuang <microhuang@live.com>
2013-09-08 16:12:29 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -25,3 +25,24 @@ if __debug__:
     if len(s)>80:s=s[:77]+'...'
     print type(x),"\n",s
     
+
+
+
+##############################
+#sys.exc_info()
+#traceback.print_exc()
+#traceback.extract_stack()
+
+
+#sitecustomize.py
+import os
+def hinfo(type, value, tb):
+  if hasattr(sys, 'ps1') or not (sys.stderr.isatty() and sys.stdin.isatty()) or issubclass(type, SyntaxError):
+    '''交互模式、wutty设备、语法错误，默认处理'''
+    sys.__excepthook__(type,value,tb)
+  else:
+    import traceback, pdb
+    traceback.print_exception(type, value, tb)
+    print
+    pdb.pm()
+sys.excepthook=hinfo
