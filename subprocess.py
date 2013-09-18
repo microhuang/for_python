ff8ed7427d99cd1b5dfd5956ad4282067b18801f
microhuang <microhuang@live.com>
2013-09-18 20:36:27 +0800

    Update subprocess.py

--- subprocess.py
+++ subprocess.py
@@ -2,12 +2,17 @@ import subprocess
 
 subp=subprocess.Popen('command', shell=True, stdout=subprocess.PIPE)
 
-#注意管道缓冲区满的问题
+#注意管道缓冲区满的问题 python -u ---- unbuffer
 
 print subp.pid
-print subp.wait()
+
+c=subp.stdout.readline()
+while c:
+  print c
+  c=subp.stdout.readline()
+  
+subp.wait()
 print subp.returncode
-print subp.stdout.readlines()
 
 
 #http://docs.python.org/2/library/subprocess.html#module-subprocess
