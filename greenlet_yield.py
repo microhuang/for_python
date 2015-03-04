ec40f15556fbaf3d0921550d1cf788776e19ac36
think <microhuang@live.com>
2015-03-04 14:21:30 +0800

    Update greenlet_yield.py

---
+++ greenlet_yield.py
@@ -0,0 +1,25 @@
+#协程库的使用
+
+import greenlet
+
+def test1():
+  print(12)
+  gr2.switch()
+  print(34)
+  
+def test2():
+  print(56)
+  gr1.switch()
+  print(78)
+
+gr1 = greenlet.greenlet(test1)
+gr2 = greenlet.greenlet(test2)
+
+gr1.swtich()
+
+'''
+output:
+12
+56
+34
+'''
