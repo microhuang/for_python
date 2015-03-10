d83e4136c3275c7a0ea06931c3393120fa1c3613
think <microhuang@live.com>
2015-03-10 18:47:59 +0800

    Update decorator&metaclass.py

--- decorator&metaclass.py
+++ decorator&metaclass.py
@@ -39,6 +39,7 @@ def square_diff(a, b):
 
 
 def decorator(F):
+    @functools.wraps(F)
     def new_F(a, b):
         print("input", a, b)
         return F(a, b)
@@ -65,9 +66,10 @@ print(square_diff(3, 4))
 
 # a new wrapper layer
 
-def pre_str(pre=''):
+def pre_str(pre=None):
     # old decorator
     def decorator(F):
+        @functools.wraps(F)
         def new_F(a, b):
             print(pre + "input", a, b)
             return F(a, b)
@@ -78,3 +80,39 @@ def pre_str(pre=''):
 @pre_str('^_^')
 def square_sum(a, b):
     return a**2 + b**2
+    
+    
+# demo
+
+def mean(frist, second, *rest):
+    numbers = (first, second) + rest
+    return sum(numbers)/len(numbers)
+    
+mean = float_args_and_return(mean)
+
+
+def float_args_and_return(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        args = [float(arg) for arg in args]
+        return float(function(*args, **kwargs))
+    return wrapper
+    
+    
+def statically_typed(*types, return_type=None):
+    def decorator(function):
+        @functools.wraps(function):
+        def wrapper(*args, *kwargs):
+            if len(args)>len(types):
+                raise ValueError("too many arguments")
+            elif len(args)<len(types):
+                raise ValueError("too few arguments")
+            for i, (arg, type_) in enumerate(zip(args, types)):
+                if not isinstance(arg, type_):
+                    raise ValueError("argument {} must be of type {}".format(i, type_.__name__))
+            result = function(*args, **kwargs)
+            if (return_type is not None and not isinstance(result, return_type)):
+                raise ValueError("return value must be of type {}".format(return_type.__name))
+            return result
+        return wrapper
+    return decorator
