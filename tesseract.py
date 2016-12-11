68bf08c2b19ac6f4a1cfd56710806067413192f6
think <microhuang@live.com>
2016-12-11 11:06:01 +0800

    Create tesseract.py

---
+++ tesseract.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import subprocess
+
+def cleanImage(filePath, newFilePath):
+  image = Image.open(filePath)
+  #对图片阈值过滤
+  image = image.point(lambda x: 0 if x<143 else 255)
+  image.save(newFilePath)
+  #OCR识别
+  subprocess.call(["tesseract",newFilePath,"output"])
+  #获取结果
+  outputFile = open("output.txt",'r')
+  print(outputFile.read())
+  outputFile.close()
+  
+cleanFile('text_2.jpg','text_2_clean.png')
